3d00cb72972a05565a095d1b54c86218f9e5e694
rafael.arcangelo@gmail.com <92927578+rafael-arcangelo@users.noreply.github.com>
2026-04-10 19:21:51 -0300

    feat(database): adiciona nova opção à coluna ENUM na query SQL

--- database/schema.sql
+++ database/schema.sql
@@ -14,10 +14,12 @@ CREATE TABLE produto (
 	id_produto INT AUTO_INCREMENT PRIMARY KEY,
     nome_produto VARCHAR(250) NOT NULL,
     descricao TEXT,
-    categoria ENUM ('entrada', 'principal', 'bebida', 'sobremesa') NOT NULL DEFAULT 'principal',
+    categoria ENUM ('entrada', 'principal', 'bebida', 'sobremesa', 'combo') NOT NULL DEFAULT 'principal',
     preco DECIMAL(10,2) NOT NULL,
     disponibilidade TINYINT NOT NULL DEFAULT 1,
     imagem_produto VARCHAR(255) NOT NULL,
     id_usuario INT NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
 ) ENGINE=InnoDB;
+
+SELECT * FROM usuario;
