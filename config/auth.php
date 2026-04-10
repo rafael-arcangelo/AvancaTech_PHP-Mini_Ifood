7109c9d1e5b46af5172b0667071c0a5d00e826fb
rafael.arcangelo@gmail.com <92927578+rafael-arcangelo@users.noreply.github.com>
2026-04-10 11:00:53 -0300

    refactor: move headers de redirecionamento para o topo e adiciona meta refresh por compatibilidade

--- config/auth.php
+++ config/auth.php
@@ -10,6 +10,7 @@
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="refresh" content="3;url=../public/login.php">
                 <title>Acesso Negado</title>
                 <link rel="stylesheet" href="../css/style.css">
             </head>
